92edc292a061f5a0d9b5fad9b87f459e2cd6fa31
Vamsi manohar <vamsimonohar@gmail.com>
2022-06-23 01:22:17 +0530

    added source code files

---
+++ dashboard-task/javascript/toggle.js
@@ -0,0 +1,43 @@
+function toggle1(){
+    var x = document.getElementById("div1");
+
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block"
+    }
+}
+
+function toggle2(){
+    var y = document.getElementById("div2");
+
+    if (y.style.display === "block"){
+        y.style.display = "none";
+    }
+    else{
+        y.style.display = "block"
+    }
+}
+
+function toggle3(){
+    var z = document.getElementById("div3");
+
+    if (z.style.display === "block"){
+        z.style.display = "none";
+    }
+    else{
+        z.style.display = "block"
+    }
+}
+
+function toggle4(){
+    var a = document.getElementById("div4");
+
+    if (a.style.display === "block"){
+        a.style.display = "none";
+    }
+    else{
+        a.style.display = "block"
+    }
+}
